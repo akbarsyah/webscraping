eae014666907f9b19bd4c3a1af2b71ce76af82a7
Akbarsyah <dhaifullahs@gmail.com>
2021-03-29 08:45:30 +0700

    koneksi ke database

---
+++ connect.php
@@ -0,0 +1,8 @@
+<?php
+    $server = 'localhost';
+    $user = 'root';
+    $pass = '';
+    $database = 'webscraping';
+
+    $con = mysqli_connect($server, $user, $pass, $database) or die('Error Tidak Dapat Terkoneksi dengan Database');
+?>
